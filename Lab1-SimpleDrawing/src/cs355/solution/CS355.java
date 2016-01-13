fc8ea67609c9082939d2d03f0b686e7041ee79e1
zjensen <zsjensen@gmail.com>
2016-01-13 08:36:36 -0700

    Finished View

--- Lab1-SimpleDrawing/src/cs355/solution/CS355.java
+++ Lab1-SimpleDrawing/src/cs355/solution/CS355.java
@@ -2,7 +2,6 @@ package cs355.solution;
 
 import cs355.GUIFunctions;
 import cs355.controller.Controller;
-import cs355.view.View;
 
 /**
  * This is the main class. The program starts here.
@@ -15,10 +14,10 @@ public class CS355 {
 	 * This is where it starts.
 	 * @param args = the command line arguments
 	 */
-	public static void main(String[] args) {
-
-		// Fill in the parameters below with your controller and view.
-		GUIFunctions.createCS355Frame(new Controller(), new View());
+	public static void main(String[] args) 
+	{
+		Controller c = new Controller();
+		GUIFunctions.createCS355Frame(c, c.getView());
 
 		GUIFunctions.refresh();
 	}
--- Lab1-SimpleDrawing/src/cs355/view/View.java
+++ Lab1-SimpleDrawing/src/cs355/view/View.java
@@ -1,27 +1,116 @@
 package cs355.view;
 
 import java.awt.Graphics2D;
+import java.util.ArrayList;
 import java.util.Observable;
+
+import cs355.GUIFunctions;
+import cs355.model.drawing.*;
 
 public class View implements ViewRefresher {
 
 	public View()
 	{
-		// TODO Auto-generated constructor stub
+		Drawing.instance().addObserver(this);
 	}
 
 	@Override
 	public void update(Observable o, Object arg)
 	{
-		// TODO Auto-generated method stub
-
+		GUIFunctions.refresh();
 	}
 
 	@Override
 	public void refreshView(Graphics2D g2d)
 	{
-		// TODO Auto-generated method stub
+		ArrayList<Shape> shapes = (ArrayList<Shape>) Drawing.instance().getShapes();
+		for (Shape shape : shapes) 
+		{
+			g2d.setColor(shape.getColor()); //set color of shape we are going to draw
+			switch (shape.getShapeType()) 
+			{
+				case LINE:
+					this.drawLine(g2d, (Line)shape);
+					break;
+				case ELLIPSE:
+					this.drawEllipse(g2d, (Ellipse)shape);
+					break;
+				case RECTANGLE:
+					this.drawRectangle(g2d, (Rectangle)shape);
+					break;
+				case CIRCLE:
+					this.drawCircle(g2d, (Circle)shape);
+					break;
+				case SQUARE:
+					this.drawSquare(g2d, (Square)shape);
+					break;
+				case TRIANGLE:
+					this.drawTriangle(g2d, (Triangle)shape);
+					break;
+				default:
+					break;
+			}
+		}
+	}
+	
+	private void drawCircle(Graphics2D g2d, Circle c) 
+	{
+		double radius = c.getRadius();
+		
+		int x = (int) (c.getCenter().getX() - radius);
+		int y = (int) (c.getCenter().getY() - radius);
+		int width = (int) (2*radius);
+		int height = width;
+		
+		g2d.fillOval(x, y, width, height);
+	}
 
+	private void drawEllipse(Graphics2D g2d, Ellipse e) 
+	{
+		int height = (int)e.getHeight();
+		int width = (int)e.getWidth();
+		int x = (int) (e.getCenter().getX() - (width / 2));
+		int y = (int) (e.getCenter().getY() - (height / 2));
+		
+		g2d.fillOval(x, y, width, height);
+	}
+	
+	private void drawLine(Graphics2D g2d, Line l) 
+	{
+		int x1 = (int)l.getStart().getX();
+		int y1 = (int)l.getStart().getY();
+		int x2 = (int)l.getEnd().getX();
+		int y2 = (int)l.getEnd().getY();
+		
+		g2d.drawLine(x1, y1, x2, y2);
+	}
+	
+	private void drawRectangle(Graphics2D g2d, Rectangle r) 
+	{
+		int x = (int) r.getUpperLeft().getX();
+		int y = (int) r.getUpperLeft().getY();
+		int width = (int) r.getWidth();
+		int height = (int) r.getHeight();
+		
+		g2d.fillRect(x, y, width, height);
+	}
+	
+	private void drawSquare(Graphics2D g2d, Square s) 
+	{
+		int x = (int) s.getUpperLeft().getX();
+		int y = (int) s.getUpperLeft().getY();
+		int width = (int) s.getSize();
+		int height = width;
+		
+		g2d.fillRect(x, y, width, height);
+	}
+	
+	private void drawTriangle(Graphics2D g2d, Triangle t) 
+	{
+		int x_coordinates[] = {(int)t.getA().getX(), (int)t.getB().getX(), (int)t.getC().getX()};
+		int y_coordinates[] = {(int)t.getA().getY(), (int)t.getB().getY(), (int)t.getC().getY()};
+		
+		g2d.fillPolygon(x_coordinates, y_coordinates, 3);
 	}
 
 }
