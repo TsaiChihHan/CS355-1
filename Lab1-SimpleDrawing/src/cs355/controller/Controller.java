e0bcde37d4881977dbc13b34f344f94af07d8fe6
zjensen <zsjensen@gmail.com>
2016-01-12 21:10:49 -0700

    Completed Controller

--- Lab1-SimpleDrawing/src/cs355/controller/Controller.java
+++ Lab1-SimpleDrawing/src/cs355/controller/Controller.java
@@ -2,154 +2,211 @@ package cs355.controller;
 
 import java.awt.Color;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.Point2D;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Controller implements CS355Controller {
+import cs355.GUIFunctions;
+import cs355.model.drawing.*;
+import cs355.view.View;
+import cs355.view.ViewRefresher;
+
+public class Controller implements CS355Controller, MouseListener, MouseMotionListener  {
+	
+	private View view;
+	private ArrayList<Point2D.Double> trianglePoints;
+	private int currentShapeIndex = -1;
+	private Point2D.Double currentMouselocation;
 
 	public Controller()
 	{
-		// TODO Auto-generated constructor stub
+		this.view = new View(); //creates view, which attaches itself as listener to model
+		this.trianglePoints = new ArrayList<Point2D.Double>(); //where we store mouse presses that will form a triangle
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e)
 	{
-		// TODO Auto-generated method stub
-
+		if(Drawing.instance().getCurrentShape().equals(Shape.type.TRIANGLE))
+		{
+			this.trianglePoints.add(new Point2D.Double(e.getX(), e.getY()));
+			if (this.trianglePoints.size() == 3) //user placed final point needed to draw triangle
+			{
+				Drawing.instance().addShape(new Triangle(Drawing.instance().getCurrentColor(), trianglePoints.get(0), trianglePoints.get(1), trianglePoints.get(2)));
+				trianglePoints.clear();
+			}
+		}
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e)
 	{
-		// TODO Auto-generated method stub
-
+		int x = e.getX();
+		int y = e.getY();
+		Point2D.Double point = new Point2D.Double((double)x, (double)y);
+		switch (Drawing.instance().getCurrentShape()) 
+		{
+			case CIRCLE:
+				this.currentMouselocation = new Point2D.Double((double)x, (double)y);
+				this.currentShapeIndex = Drawing.instance().addShape(new Circle(Drawing.instance().getCurrentColor(), point, 0));
+				break;
+			case ELLIPSE:
+				this.currentMouselocation = new Point2D.Double((double)x, (double)y);
+				this.currentShapeIndex = Drawing.instance().addShape(new Ellipse(Drawing.instance().getCurrentColor(), point, 0, 0));
+				break;
+			case LINE:
+				this.currentMouselocation = new Point2D.Double((double)x, (double)y);
+				this.currentShapeIndex = Drawing.instance().addShape(new Line(Drawing.instance().getCurrentColor(), point, point));
+				break;
+			case RECTANGLE:
+				this.currentMouselocation = new Point2D.Double((double)x, (double)y);
+				this.currentShapeIndex = Drawing.instance().addShape(new Rectangle(Drawing.instance().getCurrentColor(), point, 0, 0));
+				break;
+			case SQUARE:
+				this.currentMouselocation = new Point2D.Double((double)x, (double)y);
+				this.currentShapeIndex = Drawing.instance().addShape(new Square(Drawing.instance().getCurrentColor(), point, 0));
+				break;
+			default:
+				break;
+		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e)
 	{
-		// TODO Auto-generated method stub
-
+		Shape.type currentShape = Drawing.instance().getCurrentShape();
+		if (currentShape != null && this.currentShapeIndex!=-1) 
+		{	
+			switch (currentShape) 
+			{
+				case CIRCLE:
+				case ELLIPSE:
+				case LINE:
+				case RECTANGLE:
+				case SQUARE:
+					this.currentShapeIndex=-1;
+					this.currentMouselocation=null;
+					break; //if we were manipulating  any of the above shapes, releasing mouse signals we are finished
+				default:
+					break;
+			}
+		}
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e)
 	{
-		// TODO Auto-generated method stub
-
+		if (this.currentShapeIndex != -1) //if we are currently manipulating a shape
+		{
+			Drawing.instance().updateShape(this.currentShapeIndex, this.currentMouselocation, e);	
+		}
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void colorButtonHit(Color c)
 	{
-		// TODO Auto-generated method stub
-
+		Drawing.instance().setCurrentColor(c);
+		GUIFunctions.changeSelectedColor(c);
+		GUIFunctions.refresh();
 	}
 
 	@Override
 	public void lineButtonHit()
 	{
-		// TODO Auto-generated method stub
-
+		Drawing.instance().setCurrentShape(Shape.type.LINE);
+		trianglePoints.clear();
 	}
 
 	@Override
 	public void squareButtonHit()
 	{
-		// TODO Auto-generated method stub
-
+		Drawing.instance().setCurrentShape(Shape.type.SQUARE);
+		trianglePoints.clear();
 	}
 
 	@Override
 	public void rectangleButtonHit()
 	{
-		// TODO Auto-generated method stub
-
+		Drawing.instance().setCurrentShape(Shape.type.RECTANGLE);
+		trianglePoints.clear();
 	}
 
 	@Override
 	public void circleButtonHit()
 	{
-		// TODO Auto-generated method stub
-
+		Drawing.instance().setCurrentShape(Shape.type.CIRCLE);
+		trianglePoints.clear();
 	}
 
 	@Override
 	public void ellipseButtonHit()
 	{
-		// TODO Auto-generated method stub
-
+		Drawing.instance().setCurrentShape(Shape.type.ELLIPSE);
+		trianglePoints.clear();
 	}
 
 	@Override
 	public void triangleButtonHit()
 	{
-		// TODO Auto-generated method stub
-
+		Drawing.instance().setCurrentShape(Shape.type.TRIANGLE);
+		trianglePoints.clear();
 	}
 
 	@Override
 	public void selectButtonHit()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void zoomInButtonHit()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void zoomOutButtonHit()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void hScrollbarChanged(int value)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void vScrollbarChanged(int value)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void openScene(File file)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -163,126 +220,113 @@ public class Controller implements CS355Controller {
 	public void keyPressed(Iterator<Integer> iterator)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void openImage(File file)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void saveImage(File file)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void toggleBackgroundDisplay()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void saveDrawing(File file)
 	{
-		// TODO Auto-generated method stub
-
+		Drawing.instance().save(file);
 	}
 
 	@Override
 	public void openDrawing(File file)
 	{
-		// TODO Auto-generated method stub
-
+		Drawing.instance().open(file);
 	}
 
 	@Override
 	public void doDeleteShape()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void doEdgeDetection()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void doSharpen()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void doMedianBlur()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void doUniformBlur()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void doGrayscale()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void doChangeContrast(int contrastAmountNum)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void doChangeBrightness(int brightnessAmountNum)
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void doMoveForward()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void doMoveBackward()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void doSendToFront()
 	{
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void doSendtoBack()
 	{
 		// TODO Auto-generated method stub
+	}
 
+	public ViewRefresher getView()
+	{
+		return view;
 	}
 
 }
